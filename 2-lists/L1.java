77460767e65043d5aaa6519810a134c9999b8239
Mansur Ziiatdinov <gltronred@gmail.com>
2022-02-18 14:41:54 +0300

    Lists practice

---
+++ 2-lists/L1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class L1 {
+    // Сумму произведений соседних элементов
+    // [1,2,3,5]
+    // -> 23
+    public int sumProd1(List<Integer> l) {
+        return 0;
+    }
+    public static void main(String[] args) {
+    }
+}
