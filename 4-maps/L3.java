f70b7fab73c248c36abb458aad7a618212dadfeb
Mansur Ziiatdinov <gltronred@gmail.com>
2022-03-04 14:41:37 +0300

    Transforming to lambda: 1

--- 4-maps/L3.java
+++ 4-maps/L3.java
@@ -1,17 +1,16 @@
 import java.util.*;
 import java.util.function.*;
 
-public class L2 {
+public class L3 {
     public static void main(String[] args) {
         List<Integer> l = new LinkedList<>();
         l.add(1);
         l.add(2);
         l.add(3);
 
-        l.replaceAll(new UnaryOperator<Integer>() {
-                public Integer apply(Integer x) {
-                    return x+3;
-                }
+        l.replaceAll((Integer x) -> {
+                System.out.println(x);
+                return x+3;
             });
         l.removeIf(new Predicate<Integer>() {
                 public boolean test(Integer x) {
