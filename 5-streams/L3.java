af7fc81654b3d95cb9df0ecaacdb578caf67c941
Mansur Ziiatdinov <gltronred@gmail.com>
2022-03-25 14:41:52 +0300

    L3

--- 5-streams/L3.java
+++ 5-streams/L3.java
@@ -3,6 +3,12 @@ import java.util.*;
 import java.util.stream.*;
 
 public class L3 {
+    // Напишите reduce, который по входному
+    // IntStream с цифрами числа получит
+    // само значение числа
+    public static int number(IntStream s) {
+        return 0;
+    }
     public static void main(String[] args) {
         // [1,2,3]
         // (((0 + 1) + 2) + 3)
@@ -10,5 +16,8 @@ public class L3 {
         System.out.println(
             Stream.of(1,2,3)
                   .reduce(0,(a,b) -> a+b));
+        // 1234
+        System.out.println(
+            number(IntStream.of(1,2,3,4)));
     }
 }
