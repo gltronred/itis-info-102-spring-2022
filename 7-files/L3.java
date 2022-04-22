08afcd94d375a48caba577e03b275bdf57519837
Mansur Ziiatdinov <gltronred@gmail.com>
2022-04-22 14:53:14 +0300

    Решение

--- 7-files/L3.java
+++ 7-files/L3.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
+import java.util.stream.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
 /*
  * Рекурсивно пройтись по всем файлам в sourceDir,
@@ -46,10 +50,36 @@ import java.util.*;
  * быть написано в консоль
  */
 public class L3 {
-    public static void task(Path sourceDir, Path targetDir) {
+    public static String getFileExt(Path path) {
+        String filename = path.getFileName().toString();
+        String[] components = filename.split("\\.");
+        if (components.length == 1) {
+            return "";
+        }
+        return components[components.length-1];
+    }
+    public static void task(Path sourceDir, Path targetDir) throws IOException {
         System.out.println("Copy by types from " +
                            sourceDir + " to " +
                            targetDir);
+        try (Stream<Path> paths = Files.walk(sourceDir)) {
+            paths
+                .filter(Files::isRegularFile)
+                .forEach(source -> {
+                        Path target = targetDir
+                            .resolve(getFileExt(source))
+                            .resolve(source.getFileName());
+                        try {
+                            Files.createDirectories(target.getParent());
+                            if (Files.exists(target)) {
+                                System.out.println("File " + target + " is overwritten");
+                            }
+                            Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+                        } catch (IOException e) {
+                            System.out.println(e);
+                        }
+                    });
+        }
     }
     public static void main(String[] args) throws IOException {
         if (args.length != 2) {
@@ -61,5 +91,24 @@ public class L3 {
         Path targetDir = Path.of(args[1]);
 
         task(sourceDir, targetDir);
+    }
+
+    @Test
+    public void simpleExtTest() {
+        assertEquals("txt", getFileExt(Path.of("some.txt")));
+        assertEquals("class", getFileExt(Path.of("some.class")));
+        assertEquals("TXT", getFileExt(Path.of("some.TXT")));
+    }
+    @Test
+    public void doubleExtTest() {
+        assertEquals("txt", getFileExt(Path.of("some.other.txt")));
+    }
+    @Test
+    public void noExtTest() {
+        assertEquals("", getFileExt(Path.of("some")));
+    }
+    @Test
+    public void noNameTest() {
+        assertEquals("git", getFileExt(Path.of(".git")));
     }
 }
