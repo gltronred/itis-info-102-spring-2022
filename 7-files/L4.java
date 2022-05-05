13918eeff62d76080d322c508bf9a73afb45b216
Mansur Ziiatdinov <gltronred@gmail.com>
2022-05-05 16:42:54 +0300

    Task2

--- 7-files/L4.java
+++ 7-files/L4.java
@@ -1,8 +1,33 @@
 import java.io.*;
+import java.net.*;
 import java.nio.file.*;
 import java.util.*;
 
 public class L4 {
+    public static void task2() throws IOException {
+        PrintWriter wrong = new PrintWriter(new FileOutputStream("wrong.txt"));
+        Files.lines(Path.of("input.txt"))
+            .map(s -> Arrays.stream(s.split(" +"))
+                         .mapToInt(Integer::parseInt)
+                         .toArray())
+            .forEach(xs -> {
+                    if (xs.length == 3) {
+                        String url = "https://placekitten.com/" + xs[1] + "/" + xs[2];
+                        try (InputStream fis = new URL(url).openStream()) {
+                            Path target = Path.of(xs[0] + ".jpg");
+                            Files.copy(fis, target, StandardCopyOption.REPLACE_EXISTING);
+                        } catch (IOException e) {
+                            System.out.println(e);
+                        }
+                    } else {
+                        for (int x : xs) {
+                            wrong.print(x + " ");
+                        }
+                        wrong.println();
+                    }
+                });
+        wrong.close();
+    }
     public static int task3() throws IOException {
         Scanner sc = new Scanner(new FileInputStream("dir.txt"));
         String dir = sc.nextLine();
@@ -21,6 +46,7 @@ public class L4 {
             .sum();
     }
     public static void main(String[] args) throws IOException {
+        task2();
         System.out.println(task3());
     }
 }
