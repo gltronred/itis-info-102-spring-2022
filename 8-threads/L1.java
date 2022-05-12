7714b33757ae19af0efb1ffa0cb72ed9e30c1dd2
Mansur Ziiatdinov <gltronred@gmail.com>
2022-05-12 16:58:26 +0300

    Saving to list and using Thread.join

--- 8-threads/L1.java
+++ 8-threads/L1.java
@@ -1,17 +1,38 @@
+import java.util.*;
+
+class MyThread extends Thread {
+    private int x;
+    private LinkedList<Integer> l;
+    public MyThread(int x, LinkedList<Integer> l) {
+        this.x = x;
+        this.l = l;
+    }
+    public void run() {
+        try {
+            Thread.sleep(x*500);
+        } catch (InterruptedException e) {}
+        l.add(x);
+    }
+}
 
 public class L1 {
-    public static void timeSort(int[] a) {
-        for (int x: a) {
-            new Thread(() -> {
-                    try {
-                        Thread.sleep(x*500);
-                    } catch (InterruptedException e) {}
-                    System.out.println(x+" ");
-            }).start();
+    public static LinkedList<Integer> timeSort(int[] a) {
+        LinkedList<Integer> l = new LinkedList<>();
+        MyThread[] threads = new MyThread[a.length];
+        for (int i=0; i<a.length; i++) {
+            threads[i] = new MyThread(a[i],l);
+            threads[i].start();
         }
+        try {
+            for (Thread t : threads) {
+                t.join();
+            }
+        } catch (InterruptedException e) {}
+        return l;
     }
     public static void main(String[] args) {
         int[] a = {5, 7, 20, 1};
-        timeSort(a);
+        LinkedList<Integer> res = timeSort(a);
+        System.out.println(res);
     }
 }
